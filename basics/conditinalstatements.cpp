16ca65454811277a8c4295f7aeb40b5f1304edc5
Shreevatsatg <tgshreevatsa@gmail.com>
2024-11-15 11:18:27 +0530

    added conditional statements and loops pratice file

---
+++ basics/conditinalstatements.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int i=1;
+
+//if else block
+// cout<<"enter value for i ";
+// cin>>i;
+// if(i<=5){ 
+//     cout<<"less than 5";
+// }
+// else{
+// cout<<"greater than 5";
+// }
+
+//whie loop
+// while (i<=5){
+//     cout<<i<<endl;
+//     i++;
+// }
+
+//for loop
+i=0;
+for(i=0;i<5;i++){
+    cout<<i<<endl;
+}
+
+
+return 0;
+}
