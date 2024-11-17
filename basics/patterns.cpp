bafd051f4f66700f53eaa42b365b07d33fda14ad
Shreevatsatg <tgshreevatsa@gmail.com>
2024-11-17 18:39:37 +0530

    added pattern practice questions

--- basics/patterns.cpp
+++ basics/patterns.cpp
@@ -66,16 +66,84 @@ int main(){
 // cout<<endl;
 // }
 
+// int n=4;
+// for(int i=0;i<n;i++){
+//     for (int j=1;j<i+1;j++){
+//         cout<<j;
+//     }
+//     cout<<endl;
+// }
+
+// int c=1;
+// int n=4;
+// for(int i=0;i<n;i++){
+//     for(int j=0;j<=i;j++){
+//         cout<<c;
+//         c++;
+//     }
+//     cout<<endl;
+// }
+
+
+// int n=4;
+// for(int i=0;i<n;i++){
+//     for(int j=0;j<i;j++){
+//         cout<<" ";
+//     }
+//     for(int k=0;k<=n-i;k++){
+//         cout<<i+1;
+//     }
+//     cout<<endl;
+// }
+
+
+// int n=8;
+// for(int i=0;i<n;i++){
+//     for(int j=0;j<n-i-1;j++){
+//         cout<<" ";
+//     }
+//     for(int j=1;j<=i+1;j++){
+//         cout<<j;
+//     }
+//     int a=1;
+//     for(int k=1;k<=i+1;k++){
+//         cout<<a;
+//         a++;
+//     }
+//     cout<<endl;
+// }
+
 int n=4;
 for(int i=0;i<n;i++){
-    for (int j=1;j<i+1;j++){
-        cout<<j;
+    for(int j=0;j<n-i-1;j++){
+        cout<<" ";
     }
-    cout<<endl;
+
+cout<<"*";
+
+if(i!= 0){
+    for(int j=0;j<2*i-1;j++){
+        cout<<" ";
+    }
+    cout<<"*";
+}
+cout<<endl;
 }
 
+for(int i=0;i<(n-1);i++){
+for(int j=0;j<i+1;j++){
+        cout<<" ";
+    }
+    cout<<"*";
+if(i!=n-2){
+    for(int j=0;j<2*(n-i)-5;j++){
+        cout<<" ";
+    }
+cout<<"*";
+    }
 
+cout<<endl;
+}
     return 0;
 }
 
-//till lecture 4 ,52:40 mins
