09b60671d5e22e6143cd7efad1468aa314c1b111
shreevatsatg <tgshreevatsa@gmail.com>
2025-01-29 11:12:33 +0530

    added array practice files

---
+++ dsa/array.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int i;
+int marks[5];
+
+//int price[4]={1,3,4,5,};
+
+cout<<"enter marks into array :"<<endl;
+
+for (i =0;i<5;i++)
+{
+cin>>marks[i];
+};
+
+cout<<"output is"<<endl;
+for (i =0;i<5;i++)
+{
+cout<<marks[i]<<endl;
+};
+
+    return 0;
+}; 
