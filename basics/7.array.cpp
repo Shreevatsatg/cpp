4cd381b74862233b8b7b0aa5653e45b3487b3556
Shreevatsatg <tgshreevatsa@gmail.com>
2026-06-16 10:50:40 +0530

    small fix

--- basics/7.array.cpp
+++ basics/7.array.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
     // defination
-    int arr[5] = {1, 2, 3, 4, 5}; 
+    int arr[5] = {1, 2, 3, 4, 5};
 
     // accessing
-    cout << arr[0]; 
+    cout << arr[0];
 
-    //loop
+    // loop
     for (int i = 0; i < 5; i++)
     {
         cout << arr[i] << " ";
     }
-    
-    //size
-    int n = sizeof(arr)/sizeof(arr[0]);
+
+    // size
+    int n = sizeof(arr) / sizeof(arr[0]);
 }
