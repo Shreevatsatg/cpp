9925741aab6c513de3b0ab7748ac4b0df65a1088
Shreevatsatg <tgshreevatsa@gmail.com>
2026-07-21 09:30:11 +0530

    added 3 sum algortithm

---
+++ 8.two_pointers/4.3sum.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+#include <algorithm>
+
+using namespace std;
+
+//leet code problem no :15. 3Sum
+
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
+
+        for(int i=0;i<n;i++){
+            if(i>0 &&nums[i]==nums[i-1]) continue;
+            int j=i+1,k=n-1;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum<0){
+                    j++;
+                }else if(sum>0){
+                    k--;
+                }else{
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    j++;k--;
+
+                    while(j<k &&nums[j]==nums[j-1]) j++;
+                }
+            }
+        }
+        return ans;
+    }
+
+    int main(){
+        vector<int> nums={-1,0,1,2,-1,-4};
+        vector<vector<int>> ans=threeSum(nums);
+        for(int i=0;i<ans.size();i++){
+            for(int j=0;j<ans[i].size();j++){
+                cout<<ans[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+
+
+        return 0;
+    }
